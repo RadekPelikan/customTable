f7e772d68b9482af3851c11b50341f6baa19aef8
Radek Pelikan <pelikan.radek03@gmail.com>
2025-02-19 23:13:12 +0100

    custom hook done

--- src/hooks/movie.ts
+++ src/hooks/movie.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { MovieList } from "../types/Movie";
+import { Movie, MovieList } from "../types/Movie";
 import axios from "axios";
 import { RequestState } from "../types/Request";
 
@@ -15,6 +15,7 @@ export const useMovieData = (id?: string) => {
   const [movies, setMovies] = useState<MovieList>([]);
   const [reqState, setReqState] = useState(RequestState.Pending);
 
+  // Fetch new movies or new movie (based on ID if provided) on call of the custom useMovieData Hook
   const fetchData = async () => {
     try {
       const response = await axiosClient.get("Film.JSON");
@@ -35,5 +36,47 @@ export const useMovieData = (id?: string) => {
     });
   }, []);
 
-  return { movies, setMovies, reqState };
+  // Delete a movie based on id
+  const deleteMovie = async (id: string) => {
+    setReqState(RequestState.Pending);
+    try {
+      await axiosClient.delete(id);
+      setMovies((prev) => prev.filter((movie) => movie.imdbID != id));
+    } catch (e) {
+      setReqState(RequestState.Failed);
+    }
+  };
+
+  // Update a movie based on a movie (Movie to update is determined by id within it)
+  const updateMovie = async (movie: Movie) => {
+    setReqState(RequestState.Pending);
+    try {
+      const request = await axiosClient.put(movie.imdbID, movie);
+      const newMovie = request.data as Movie;
+      const newMovieList = movies.filter(
+        (_movie) => _movie.imdbID != movie.imdbID
+      );
+      newMovieList.push(newMovie);
+      setMovies(newMovieList);
+    } catch (e) {
+      setReqState(RequestState.Failed);
+    }
+  };
+
+  // Insert a new movie to database
+  const createMovie = async (movie: Movie) => {
+    setReqState(RequestState.Pending);
+    try {
+      const request = await axiosClient.post("/", movie)
+      const newMovie = request.data as Movie;
+      setMovies(prev => {
+        prev.push(newMovie);
+        return prev
+      })
+    } catch (e) {
+      setReqState(RequestState.Failed);
+    }
+  }
+
+  return { movies, deleteMovie, updateMovie, createMovie, reqState };
 };
